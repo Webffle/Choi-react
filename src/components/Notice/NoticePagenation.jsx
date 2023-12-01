409d10fb6fd124946ba23d5aa22db7fb998901c7
최현 <choehyeon@choehyeon-ui-noteubug.local>
2023-12-01 09:37:51 +0900

    feat: Pagenation 진행 중

--- src/components/Notice/NoticePagenation.jsx
+++ src/components/Notice/NoticePagenation.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
 import style from "../../css/Notice.module.css";
 
 const NoticePagenation = () => {
+  const [page, setPage] = useState([]);
+
+  const createPageArray = (num) => {
+    let result = [];
+
+    for (let i = 1; i < num + 1; i++) {
+      result.push(i);
+    }
+    return result;
+  };
+
+  useEffect(() => {
+    const test2 = 24 / 5;
+
+    setPage(createPageArray(test2));
+  }, []);
+
   return (
     <div className={style.pagenation}>
       <button>이전</button>
-      <button className={style.pagenation__number}>1</button>
-      <button className={style.pagenation__number}>2</button>
+      {page.map((page) => (
+        <button className={style.pagenation__number}>{page}</button>
+      ))}
       <button>다음</button>
     </div>
   );
